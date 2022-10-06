47a2ba0486ce3df9d75fe25cf368df497231475e
mfernandezfreire <mfernandezfreir.ext@acciona.com>
2022-10-06 13:49:19 +0200

    npm installed & ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from 'mff-product-card';
+import { Button, MyLabel } from 'mff-my-storybook-components';
+
+const product1 = {
+  id: '1',
+  title: 'Coffe Mug - Sin Imagen'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+
+      <h1>Consumiendo mi primer paquete NPM</h1>
+
+      <ProductCard
+        product={product1}
+        initialValues={{
+          count: 4,
+          maxCount: 15
+        }}
+      >
+        {
+          ({ count, maxCount, isMaxCountReached, reset, increaseBy }) => (
+            <>
+              <ProductImage />
+              <ProductTitle />
+              <ProductButtons />
+            </>
+          )
+        }
+      </ ProductCard >
+
+      <h1>Consumiendo mi segundo paquete con storybook</h1>
+        <MyLabel label="MyLabel npm" backgroundColor="white" size="h1" />
+        <Button label="Hello from NPM" backgroundColor="white"/>
     </div>
   );
 }
